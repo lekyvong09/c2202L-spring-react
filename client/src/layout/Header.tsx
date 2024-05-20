37ee0ce6fb2500fecbec50980ddc7f6dbca56f76
lekyvong09 <lekyvong09@gmail.com>
2024-05-20 21:35:43 +0700

    2024 May 20 -2

--- client/src/layout/Header.tsx
+++ client/src/layout/Header.tsx
@@ -1,4 +1,5 @@
-import { AppBar, List, ListItem, Switch, Toolbar, Typography } from "@mui/material";
+import { ShoppingCart } from "@mui/icons-material";
+import { AppBar, Badge, Box, IconButton, List, ListItem, Switch, Toolbar, Typography } from "@mui/material";
 import * as React from 'react';
 import { NavLink } from "react-router-dom";
 
@@ -24,28 +25,68 @@ export default function Header(props: Props) {
         props.onSetDarkMode(event.target.checked);
     };
 
+    const navStyles = {
+        color: 'inherit',
+        '&:hover': {
+            color: 'grey.500'
+        },
+        '&.active': {
+            color: 'text.secondary'
+        }
+    };
+
     return (
         <AppBar position="static">
-            <Toolbar>
-                <Typography variant="h6" >My Shop</Typography>
-                <Switch
-                    checked={props.darkMode}
-                    onChange={handleChange}
-                    inputProps={{ 'aria-label': 'controlled' }}
-                    color="warning"
-                />
-                <List sx={{display: 'flex'}}>
-                    {midLinks.map(({title, path}) => (
-                        <ListItem
-                            key={path}
-                            component={NavLink}
-                            to={path}
-                        >
-                            {title.toUpperCase()}
-                        </ListItem>
-                    ))}
+            <Toolbar sx={{display: 'flex', justifyContent: 'space-between', alignItems:'center'}}>
+                <Box display={'flex'} alignItems={'center'}>
+                    <Typography 
+                        variant="h6" 
+                        component={NavLink} to={'/'}
+                        sx={{color: 'inherit', textDecoration: 'none'}}
+                    >
+                        My Shop
+                    </Typography>
+                    <Switch
+                        checked={props.darkMode}
+                        onChange={handleChange}
+                        inputProps={{ 'aria-label': 'controlled' }}
+                        color="warning"
+                    />
+                    <List sx={{display: 'flex'}}>
+                        {midLinks.map(({title, path}) => (
+                            <ListItem
+                                key={path}
+                                component={NavLink}
+                                to={path}
+                                sx={navStyles}
+                            >
+                                {title.toUpperCase()}
+                            </ListItem>
+                        ))}
+                    </List>
+                </Box>
+                
+                <Box display={'flex'} alignItems={'center'}>
+                    <IconButton size="large" sx={{color:'inherit'}}>
+                        <Badge badgeContent={4} color="secondary">
+                            <ShoppingCart />
+                        </Badge>
+                    </IconButton>
 
-                </List>
+                    <List sx={{display: 'flex'}}>
+                        {rightLinks.map(({title, path}) => (
+                            <ListItem
+                                key={path}
+                                component={NavLink}
+                                to={path}
+                                sx={navStyles}
+                            >
+                                {title.toUpperCase()}
+                            </ListItem>
+                        ))}
+                    </List>
+                </Box>
+                
             </Toolbar>
         </AppBar>
     );
